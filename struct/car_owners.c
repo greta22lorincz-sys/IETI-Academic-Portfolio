c97437ed67a47092498aa97e7272191717baa07d
Lorint Greta <greta22lorincz@gmail.com>
2026-02-08 20:44:52 +0200

    car_owners.c

--- struct/car_owners.c
+++ struct/car_owners.c
@@ -1,82 +1,89 @@
+/*
+ * Project: Car Owner Database
+ * File: car_owners.c
+ * Description:
+ *   This program reads information about several people, identifies the
+ *   unmarried owner of the newest DACIA, and sorts all names alphabetically.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct {
-    char nume[50]; 
-    int casatorit; 
-    char masina[50]; 
-    int an; 
-}DATE; 
+    char name[50];
+    int married;        // 0 = no, 1 = yes
+    char carBrand[50];
+    int year;
+} PERSON;
 
-int main()
-{
-      printf("introduceti elementele tabloului: "); 
-      int n; 
-      scanf("%d", &n); 
-      getchar(); 
-      
-      DATE *v= malloc(n*sizeof(DATE)); 
-      if(!v){
-          printf("nu exista spatiu suficient"); 
-          exit(1); 
-      }
-      
-      //a 
-      for(int i=0; i<n; i++)
-      {
-          printf("introduceti datele persoanei %d: \n", i+1); 
-          printf("numele: "); 
-          scanf("%s", v[i].nume); 
-          getchar(); 
-          
-          printf("casatorit (0/1): "); 
-          scanf("%d", &v[i].casatorit); 
-          getchar(); 
-          
-          printf("marca masinii (majuscule): "); 
-          scanf("%s", v[i].masina); 
-          getchar(); 
-          
-          printf("anul de fabricatie al masinii: ");
-          scanf("%d", &v[i].an); 
-          getchar(); 
-      }
-      
-      //b 
-      int maxi=-1, poz=-1;  
-      for(int i=0; i<n; i++)
-      {
-          if(strcmp(v[i].masina, "DACIA")==0 && v[i].casatorit==0  )
-            if(v[i].an > maxi){
-              maxi = v[i].an; 
-              poz=i; 
+int main() {
+    int n;
+
+    printf("Enter the number of people: ");
+    scanf("%d", &n);
+
+    PERSON *v = malloc(n * sizeof(PERSON));
+    if (!v) {
+        printf("Error: insufficient memory.\n");
+        return 1;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter data for person %d:\n", i + 1);
+
+        printf("Name: ");
+        scanf("%s", v[i].name);
+
+        printf("Married (0 = no, 1 = yes): ");
+        scanf("%d", &v[i].married);
+
+        printf("Car brand (uppercase): ");
+        scanf("%s", v[i].carBrand);
+
+        printf("Car manufacturing year: ");
+        scanf("%d", &v[i].year);
+    }
+
+    
+    int newestYear = -1;
+    int pos = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(v[i].carBrand, "DACIA") == 0 && v[i].married == 0) {
+            if (v[i].year > newestYear) {
+                newestYear = v[i].year;
+                pos = i;
             }
-      }
-      if(poz>=0)
-        printf("proprietarul de dacia cea mai noua necasatorit este: %s\n", v[poz].nume); 
-    else printf("nu exsita astfel de pers. "); 
-      
-      
-      //c
-      int sortat; 
-      do{
-          sortat = 1; 
-          for(int i=0; i<n-1; i++)
-            if(strcmp(v[i].nume, v[i+1].nume) > 0)
-            {
-                DATE aux = v[i];
-                v[i] = v[i+1];
-                v[i+1] = aux;
-                sortat = 0;
+        }
+    }
+
+    if (pos >= 0)
+        printf("\nThe unmarried owner of the newest DACIA is: %s\n", v[pos].name);
+    else
+        printf("\nNo unmarried DACIA owner found.\n");
+
+
+    int sorted;
+    do {
+        sorted = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (strcmp(v[i].name, v[i + 1].name) > 0) {
+                PERSON aux = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = aux;
+                sorted = 0;
             }
-      }while(sortat == 0); 
-      
-     printf("tabloul sortat dupa nume: \n"); 
-     for(int i=0; i<n; i++)
-       printf("%s ", v[i].nume); 
+        }
+    } while (!sorted);
 
+    printf("\nPeople sorted alphabetically by name:\n");
+    for (int i = 0; i < n; i++)
+        printf("%s ", v[i].name);
 
-    free(v); 
+    printf("\n");
+
+    free(v);
     return 0;
 }
