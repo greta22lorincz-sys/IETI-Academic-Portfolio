eed3323d0aab77a49e1a1fa70538bf81eb40b0af
Lorint Greta <greta22lorincz@gmail.com>
2026-02-08 20:45:53 +0200

    Update computers.c

--- struct/computers.c
+++ struct/computers.c
@@ -1,83 +1,88 @@
+/*
+ * Project: Computer Inventory Manager
+ * File: computers.c
+ * Description:
+ *   This program reads information about several computers, sorts them
+ *   in descending order by manufacturing year, and then displays those
+ *   that meet specific hardware requirements.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct {
-    char procesor[30];
+    char processor[30];
     int hard_disk;
-    int memorie_ram;
-    int an;
-} CALC; 
+    int ram;
+    int year;
+} COMPUTER;
 
-void citire(CALC *v, int n){
-    for(int i=0; i<n; i++)
-    {
-        printf("date calculator %d: \n", i+1); 
-        
-        printf("procesor= "); 
-        scanf("%s", v[i].procesor); 
-        
-        printf("hard_disk= "); 
-        scanf("%d", &v[i].hard_disk); 
-        
-        printf("memorie= "); 
-        scanf("%d", &v[i].memorie_ram); 
-        
-        printf("an = "); 
-        scanf("%d", &v[i].an); 
+void readComputers(COMPUTER *v, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("\nComputer %d:\n", i + 1);
+
+        printf("Processor = ");
+        scanf("%s", v[i].processor);
+
+        printf("Hard disk (GB) = ");
+        scanf("%d", &v[i].hard_disk);
+
+        printf("RAM (GB) = ");
+        scanf("%d", &v[i].ram);
+
+        printf("Year = ");
+        scanf("%d", &v[i].year);
     }
 }
 
-void afisare(CALC *v, int n){
-    printf("calculatoarele sunt: (dupa sortare)\n");
-    for(int i=0; i<n; i++)
-    {
-        printf("%d. procesor: %s, hard disk: %d, ram: %d, anul: %d\n", i+1, v[i].procesor, v[i].hard_disk, v[i].memorie_ram, v[i].an); 
+void displayComputers(COMPUTER *v, int n) {
+    printf("\nComputers (after sorting):\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d. Processor: %s, Hard disk: %d GB, RAM: %d GB, Year: %d\n",
+               i + 1, v[i].processor, v[i].hard_disk, v[i].ram, v[i].year);
     }
 }
 
+int main(void) {
+    int n;
 
-int main(void)
-{
-    int n; 
-    printf("numarul de calc: "); 
-    scanf("%d", &n); 
-    
-    CALC *v= malloc(n*sizeof(CALC)); 
-    if(!v){
-        printf("memorie insuf"); 
-        exit(1); 
+    printf("Number of computers: ");
+    scanf("%d", &n);
+
+    COMPUTER *v = malloc(n * sizeof(COMPUTER));
+    if (!v) {
+        printf("Error: insufficient memory.\n");
+        return 1;
     }
-    
-    //a 
-    citire(v, n); 
-    
-    //b
-    //ord desc dupa an 
-    
-    int sortat; 
-    do{
-        sortat =1; 
-        for(int i=0; i<n-1; i++)
-          if(v[i].an < v[i+1].an){
-              CALC aux; 
-              aux = v[i]; 
-              v[i] = v[i+1]; 
-              v[i+1] = aux;           
-              sortat = 0; 
-          }
-    }while(sortat == 0); 
-    
-    afisare(v, n); 
-    
-    //c 
-    printf("calc in ord cresc dupa an cu ram>1 si hard disk>=256 GB: \n"); 
-    for(int i=n-1; i>=0; i--){
-        if(v[i].hard_disk>=256 && v[i].memorie_ram>1)
-          printf("procesor: %s, hard disk: %d, ram: %d, anul: %d\n", v[i].procesor, v[i].hard_disk, v[i].memorie_ram, v[i].an); 
+
+
+    readComputers(v, n);
+
+    int sorted;
+    do {
+        sorted = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (v[i].year < v[i + 1].year) {
+                COMPUTER aux = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = aux;
+                sorted = 0;
+            }
+        }
+    } while (!sorted);
+
+    displayComputers(v, n);
+
+   
+    printf("\nComputers in ascending order by year with RAM > 1 GB and HDD >= 256 GB:\n");
+    for (int i = n - 1; i >= 0; i--) {
+        if (v[i].hard_disk >= 256 && v[i].ram > 1) {
+            printf("Processor: %s, Hard disk: %d GB, RAM: %d GB, Year: %d\n",
+                   v[i].processor, v[i].hard_disk, v[i].ram, v[i].year);
+        }
     }
-   
-   
-   free(v); 
+
+    free(v);
     return 0;
 }
